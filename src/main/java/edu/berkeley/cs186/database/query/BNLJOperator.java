1f93a9b54dae84705bcac5d5a40d475e7a073c97
sandeeptiwari97 <stiwari@berkeley.edu>
2018-06-01 18:38:08 -0700

    Add files via upload

---
+++ src/main/java/edu/berkeley/cs186/database/query/BNLJOperator.java
@@ -0,0 +1,216 @@
+package edu.berkeley.cs186.database.query;
+
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import edu.berkeley.cs186.database.Database;
+import edu.berkeley.cs186.database.DatabaseException;
+import edu.berkeley.cs186.database.common.BacktrackingIterator;
+import edu.berkeley.cs186.database.databox.DataBox;
+import edu.berkeley.cs186.database.io.Page;
+import edu.berkeley.cs186.database.table.Record;
+import edu.berkeley.cs186.database.table.Schema;
+
+public class BNLJOperator extends JoinOperator {
+
+  protected int numBuffers;
+
+  public BNLJOperator(QueryOperator leftSource,
+                      QueryOperator rightSource,
+                      String leftColumnName,
+                      String rightColumnName,
+                      Database.Transaction transaction) throws QueryPlanException, DatabaseException {
+    super(leftSource,
+            rightSource,
+            leftColumnName,
+            rightColumnName,
+            transaction,
+            JoinType.BNLJ);
+
+    this.numBuffers = transaction.getNumMemoryPages();
+  }
+
+  public Iterator<Record> iterator() throws QueryPlanException, DatabaseException {
+    return new BNLJIterator();
+  }
+
+
+  /**
+   * BNLJ: Block Nested Loop Join
+   *  See lecture slides.
+   *
+   * An implementation of Iterator that provides an iterator interface for this operator.
+   *
+   * Before proceeding, you should read and understand SNLJOperator.java
+   *    You can find it in the same directory as this file.
+   *
+   * Word of advice: try to decompose the problem into distinguishable sub-problems.
+   *    This means you'll probably want to add more methods than those given (Once again,
+   *    SNLJOperator.java might prove to be a useful reference).
+   */
+  private class BNLJIterator extends JoinIterator {
+    /**
+     * Some member variables are provided for guidance, but there are many possible solutions.
+     * You should implement the solution that's best for you, using any member variables you need.
+     * You're free to use these member variables, but you're not obligated to.
+     */
+
+    private BacktrackingIterator<Page> leftIterator = null;
+    private BacktrackingIterator<Page> rightIterator = null;
+    private BacktrackingIterator<Record> leftRecordIterator = null;
+    private BacktrackingIterator<Record> rightRecordIterator = null;
+    private Record rightRecord = null;
+    private Record leftRecord = null;
+    private Record nextRecord = null;
+
+    public BNLJIterator() throws QueryPlanException, DatabaseException {
+      super();
+      rightIterator = BNLJOperator.this.getPageIterator(this.getRightTableName());
+      leftIterator = BNLJOperator.this.getPageIterator(this.getLeftTableName());
+      rightIterator.next();
+      leftIterator.next();
+
+      rightRecordIterator = BNLJOperator.this.getTransaction().getBlockIterator(this.getRightTableName(), rightIterator, 1);
+      leftRecordIterator = BNLJOperator.this.getTransaction().getBlockIterator(this.getLeftTableName(), leftIterator, numBuffers - 2);
+
+      if (leftRecordIterator.hasNext()) {
+        leftRecord = leftRecordIterator.next();
+        leftRecordIterator.mark();
+      } else {
+        leftRecord = null;
+      }
+
+      if (rightRecordIterator.hasNext()) {
+        rightIterator.mark();
+        rightRecord = rightRecordIterator.next();
+        rightRecordIterator.mark();
+      } else {
+        rightRecord = null;
+      }
+
+      try {
+        fetchNextRecord();
+      } catch (NoSuchElementException e) {
+        nextRecord = null;
+      }
+    }
+
+    private void resetRightRecord() {
+      this.rightRecordIterator.reset();
+      rightRecord = rightRecordIterator.next();
+    }
+
+    private void resetLeftRecord() {
+      this.leftRecordIterator.reset();
+      leftRecord = leftRecordIterator.next();
+    }
+
+    private void resetRightPage() throws DatabaseException {
+      rightIterator = BNLJOperator.this.getPageIterator(this.getRightTableName());
+      rightIterator.next();
+      rightRecordIterator = BNLJOperator.this.getTransaction().getBlockIterator(this.getRightTableName(), rightIterator, 1);
+      if (rightRecordIterator.hasNext()) {
+        rightRecord = rightRecordIterator.next();
+        rightRecordIterator.mark();
+      } else {
+        rightRecord = null;
+      }
+
+    }
+
+    public void fetchNextRecord() throws DatabaseException {
+      if (leftRecord == null && rightRecord == null && !leftRecordIterator.hasNext() && !rightRecordIterator.hasNext() && !leftIterator.hasNext() && !rightIterator.hasNext()) {
+        nextRecord = null;
+        return;
+      }
+      while (!(leftRecord == null && rightRecord == null && !leftRecordIterator.hasNext() && !rightRecordIterator.hasNext() && !leftIterator.hasNext() && !rightIterator.hasNext())) {
+        if (leftRecord != null) {
+          if (rightRecord != null) {
+            DataBox leftJoinValue = leftRecord.getValues().get(BNLJOperator.this.getLeftColumnIndex());
+            DataBox rightJoinValue = rightRecord.getValues().get(BNLJOperator.this.getRightColumnIndex());
+            if (leftJoinValue.equals(rightJoinValue)) {
+              List<DataBox> leftValues = new ArrayList<>(this.leftRecord.getValues());
+              List<DataBox> rightValues = new ArrayList<>(rightRecord.getValues());
+              leftValues.addAll(rightValues);
+              this.nextRecord = new Record(leftValues);
+              rightRecord = rightRecordIterator.hasNext() ? rightRecordIterator.next() : null;
+              return;
+            } else {
+              rightRecord = rightRecordIterator.hasNext() ? rightRecordIterator.next() : null;
+              continue;
+            }
+          } else {
+            if (leftRecordIterator.hasNext()) {
+              leftRecord = leftRecordIterator.next();
+              resetRightRecord();
+            } else {
+              leftRecord = null;
+            }
+          }
+        } else {
+          if (rightIterator.hasNext()) {
+            rightRecordIterator = BNLJOperator.this.getTransaction().getBlockIterator(this.getRightTableName(), rightIterator,  1);
+            if (rightRecordIterator.hasNext()) {
+              rightRecord = rightRecordIterator.next();
+              rightRecordIterator.mark();
+              resetLeftRecord();
+            } else {
+              rightRecord = null;
+            }
+          } else {
+            if (leftIterator.hasNext()) {
+              leftRecordIterator = BNLJOperator.this.getTransaction().getBlockIterator(this.getLeftTableName(), leftIterator, numBuffers - 2);
+              if (leftRecordIterator.hasNext()) {
+                leftRecord = leftRecordIterator.next();
+                leftRecordIterator.mark();
+                resetRightPage();
+              } else {
+                leftRecord = null;
+              }
+            } else {
+              nextRecord = null;
+              return;
+            }
+          }
+        }
+      }
+      nextRecord = null;
+    }
+
+    /**
+     * Checks if there are more record(s) to yield
+     *
+     * @return true if this iterator has another record to yield, otherwise false
+     */
+    public boolean hasNext() {
+      return this.nextRecord != null;
+    }
+
+    /**
+     * Yields the next record of this iterator.
+     *
+     * @return the next Record
+     * @throws NoSuchElementException if there are no more Records to yield
+     */
+    public Record next() {
+      if (!this.hasNext()) {
+        throw new NoSuchElementException();
+      }
+
+      Record nextRecord = this.nextRecord;
+      try {
+        this.fetchNextRecord();
+      } catch (DatabaseException e) {
+        this.nextRecord = null;
+      }
+      return nextRecord;
+    }
+
+    public void remove() {
+      throw new UnsupportedOperationException();
+    }
+  }
+}
